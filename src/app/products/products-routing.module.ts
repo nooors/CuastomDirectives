919318d6271f298a5ea3c83762ea6da78a1920fb
Jose Nortes <nooors@gmail.com>
2022-08-22 08:23:42 +0200

    feat: Routing set

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,7 +1,13 @@
+import { AddComponent } from './pages/add/add.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', children: [
+    { path: 'add', component: AddComponent},
+    { path: '**', redirectTo: 'add'},
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
